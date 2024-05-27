103e9f286c1fc24822faccb3a82cc0f59e7196de
Leonaru <owlyxofficial@gmail.com>
2024-05-27 22:57:25 +0700

    my changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,10 @@ import "swiper/css";
 import LoginPage from "./page/login";
 import SignupPage from "./page/signup";
 import Home from "./page/home";
-import CustomCursor from "./assets/js/customCursor";
 
 function App() {
   return (
     <BrowserRouter>
-      <CustomCursor />
       <Routes>
         <Route path="login" element={<LoginPage />}></Route>
         <Route path="signup" element={<SignupPage />}></Route>

963f7a7d2b82729b9cdac864c38a2cdc19bb95e9
dionsylvester <dionsyl89@gmail.com>
2024-06-13 03:39:12 +0700

    breakline

--- frontend/src/component/learning.jsx
+++ frontend/src/component/learning.jsx
@@ -90,7 +90,9 @@ const Learning = () => {
                         }
                     </div>
                 </div>
-                <div className="course-enroll justify-content-center">
+                <br />
+                <br />
+                <div className="course-enroll row g-4 justify-content-center row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 course-filter">
                     <Link to="/homelog" className="lab-btn"><span>Search More</span></Link>
                 </div>
             </div>
